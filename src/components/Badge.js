58ac9482b5b4cfe6b6d88dbfca00f504cbf60511
FredyReyes <fredy.reyes.a@gmail.com>
2020-06-20 09:57:57 -0500

    CursoReact
    Firts Update

---
+++ src/components/Badge.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+class Badge extends React.Component{
+    render(){
+        return(
+            <div>
+                <div>
+                    <img src="" alt="Logo conferencia"/>
+                </div>
+
+                <div>
+                    <img src="" alt="Avatar"/>
+                    <h1>John <br/> Reyes</h1>
+                </div>
+
+                <div>
+                    <p> Fronted engieniering</p>
+                </div>
+
+                <div>
+                    #platziconf
+                </div>
+            </div>
+        )
+    }
+
+}
+
+export default Badge;
--- src/index.js
+++ src/index.js
@@ -1,16 +1,37 @@
-// const element = document.createElement('h1');
-// element.innerText = 'Hello, Platzi Badges!';
-
-// const container = document.getElementById('app');
-
-// container.appendChild(element);
-
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const element = <h1>Hello, Platzi Badges!</h1>;
 
-const container = document.getElementById('app');
+import Badge from './components/Badge'
+//const jsx = <h1>Hola fredy</h1>
 
-// ReactDOM.render(__qué__, __dónde__);
-ReactDOM.render(element, container);
+// const element = React.createElement('a',
+// {href: 'https:google.com'},
+// 'ir a google');
+
+const name = 'John';
+
+const sum = () => 3+3 ; 
+// const element = React.createElement(
+//     'h1',
+//     {},
+//     `hola soy ${name}`);
+
+// const jsx = <h1>Hola soy, {sum()}</h1>
+
+// const jsx = <div>
+//     <h1>Hola soy Fredy</h1>
+//     <p>Este es un parrafo</p>
+// </div>
+
+// const element = React.createElement(
+//     'div',
+//     {},
+//     React.createElement('h1',{},'Hola soy fredy'),
+//     React.createElement('p',{},'esto es un parrafo')
+// )
+
+const container = document.getElementById('app')
+
+//ReactDOM.render(__que__, __ donde __);
+ReactDOM.render(<Badge/>,container);
